f56600a94da17b2f2bd993877f0f4f80de6106c8
nick.no.cave@gmail.com <nick.no.cave@gmail.com>
2017-11-20 21:33:31 +0200

    config modified

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 var __CONFIG__ = {
-	clientId: '732588629667-78die8b0im10dkg91o48bo7knds74r92.apps.googleusercontent.com'
+	clientId: '744392057338-qj2v1vktdqabg9hhua2ofarus5cu8gta.apps.googleusercontent.com'
 };
